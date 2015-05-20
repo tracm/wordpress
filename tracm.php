690556acbd3235c793f774dcfa4c4e0e290f14eb
Justin Dalrymple <wholehogsoftware@gmail.com>
2015-05-20 02:37:41 -0500

    Fixed relics

--- tracm.php
+++ tracm.php
@@ -24,7 +24,7 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-// Include eE API library
+// Include Tra.cm API library
 require_once "includes/TracmAPIExchange.php";
 
 // hook for adding admin menus
